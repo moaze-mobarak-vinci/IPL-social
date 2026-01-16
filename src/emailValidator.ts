b73c8ca0b5a363f078768f166f7ce7be305d6cce
Moaze Mobarak <moaze.mobarak@student.vinci.be>
2026-01-16 16:14:43 +0100

    TDD: validation de l'absence d'espaces (Vert)

--- src/emailValidator.ts
+++ src/emailValidator.ts
@@ -1,9 +1,11 @@
 export function validateEmail(email: string): boolean {
+    // Règle c : Il ne peut contenir aucun espace
+    if (email.includes(' ')) return false;
+
     // Règle a : Il doit contenir au moins un @
     if (!email.includes('@')) return false;
 
     const parts = email.split('@');
-    // On récupère la partie après le dernier @ (au cas où il y en aurait plusieurs)
     const domain = parts[parts.length - 1];
 
     // Règle b : au moins un point dans le domaine et pas à la fin
