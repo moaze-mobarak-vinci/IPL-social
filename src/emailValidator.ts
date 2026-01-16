085e7a24d8e766ce69211374f355a15be59ba7d8
Moaze Mobarak <moaze.mobarak@student.vinci.be>
2026-01-16 16:12:31 +0100

    TDD: validation du point dans le domaine (Vert)

--- src/emailValidator.ts
+++ src/emailValidator.ts
@@ -1,7 +1,15 @@
 export function validateEmail(email: string): boolean {
     // Règle a : Il doit contenir au moins un @
-    if (!email.includes('@')) {
+    if (!email.includes('@')) return false;
+
+    const parts = email.split('@');
+    // On récupère la partie après le dernier @ (au cas où il y en aurait plusieurs)
+    const domain = parts[parts.length - 1];
+
+    // Règle b : au moins un point dans le domaine et pas à la fin
+    if (!domain.includes('.') || domain.endsWith('.')) {
         return false;
     }
+
     return true;
 }
