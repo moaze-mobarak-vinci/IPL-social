d6af3507f2c5baa7eec0c48695bc8764d6a5cf0b
Moaze Mobarak <moaze.mobarak@student.vinci.be>
2026-01-16 16:15:43 +0100

    TDD: test pour texte avant et après le @ (Rouge)

--- src/tests/emailValidator.test.ts
+++ src/tests/emailValidator.test.ts
@@ -13,3 +13,9 @@ test('doit rejeter une adresse contenant des espaces', () => {
     expect(validateEmail('test @domain.com')).toBe(false);
     expect(validateEmail('test@dom ain.com')).toBe(false);
 });
+
+test('doit rejeter une adresse sans texte avant ou après le @', () => {
+    expect(validateEmail('@domain.com')).toBe(false);
+    expect(validateEmail('test@.com')).toBe(false); // Cas particulier du point seul après @
+    expect(validateEmail('test@')).toBe(false);
+});
