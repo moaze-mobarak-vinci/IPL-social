5c86efeb684a36baed45f59059f779ca36943fec
Moaze Mobarak <moaze.mobarak@student.vinci.be>
2026-01-16 16:13:12 +0100

    TDD: test pour l'interdiction des espaces (Rouge)

--- src/tests/emailValidator.test.ts
+++ src/tests/emailValidator.test.ts
@@ -8,3 +8,8 @@ test('doit rejeter une adresse sans point dans le domaine ou avec un point à la
     expect(validateEmail('test@domain')).toBe(false);
     expect(validateEmail('test@domain.')).toBe(false);
 });
+
+test('doit rejeter une adresse contenant des espaces', () => {
+    expect(validateEmail('test @domain.com')).toBe(false);
+    expect(validateEmail('test@dom ain.com')).toBe(false);
+});
