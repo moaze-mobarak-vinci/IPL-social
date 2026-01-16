34cbd0e2f26c24a935173b63a79ceb7cc43885a3
Moaze Mobarak <moaze.mobarak@student.vinci.be>
2026-01-16 16:11:22 +0100

    TDD: test pour le point dans le domaine (Rouge)

--- src/tests/emailValidator.test.ts
+++ src/tests/emailValidator.test.ts
@@ -3,3 +3,8 @@ import { validateEmail } from '../emailValidator';
 test('doit rejeter une adresse sans @', () => {
     expect(validateEmail('testatdomain.com')).toBe(false);
 });
+
+test('doit rejeter une adresse sans point dans le domaine ou avec un point à la fin', () => {
+    expect(validateEmail('test@domain')).toBe(false);
+    expect(validateEmail('test@domain.')).toBe(false);
+});
